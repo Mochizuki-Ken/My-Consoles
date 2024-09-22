9e9712b0c3fadd3fd0a55c8a167739d6059a7411
Mochizuki Ken <140779472+Mochizuki-Ken@users.noreply.github.com>
2024-09-22 12:12:53 +0800

    Update Service.js

--- src/Service/Service.js
+++ src/Service/Service.js
@@ -8,12 +8,7 @@ import 'firebase/compat/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAJg9pOsoNqLN9J_Fyshd-QvKRkFUGdJT0",
-  authDomain: "my-consoles.firebaseapp.com",
-  projectId: "my-consoles",
-  storageBucket: "my-consoles.appspot.com",
-  messagingSenderId: "636485161295",
-  appId: "1:636485161295:web:dd81aae6e8402ffe4ff53a"
+
 };
 
 // Initialize Firebase
